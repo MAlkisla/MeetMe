282534e01b73f89b7a0213bf65f0ca328cad58fa
Meriç ALKIŞLA <meric.alkisla@outlook.com.tr>
2021-01-19 13:49:10 +0300

    MeetingsAdded

--- MeetMe/Program.cs
+++ MeetMe/Program.cs
@@ -1,5 +1,8 @@
+using MeetMe.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,9 +14,17 @@ namespace MeetMe
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvder = scope.ServiceProvider;
+                var roleManager = serviceProvder.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = serviceProvder.GetRequiredService<UserManager<ApplicationUser>>();
+                await ApplicationDbContextSeeder.SeedRolesAndUsersAsync(roleManager, userManager);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
