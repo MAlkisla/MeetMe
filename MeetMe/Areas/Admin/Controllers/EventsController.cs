d23518f050a40a37e115129f82519e28cda0b159
Meriç ALKIŞLA <meric.alkisla@outlook.com.tr>
2021-01-25 10:20:42 +0300

    update

---
+++ MeetMe/Areas/Admin/Controllers/EventsController.cs
@@ -0,0 +1,57 @@
+﻿using MeetMe.Areas.Admin.Models;
+using MeetMe.Data;
+using MeetMe.Utilities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetMe.Areas.Admin.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class EventsController : AdminBaseController
+    {
+        public EventsController(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Meetings.ToList());
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult New(NewMeetingViewModel vm, [FromServices] IWebHostEnvironment env )
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = null;
+                if (vm.Photo != null && vm.Photo.Length > 0)
+                {
+                    fileName = vm.Photo.GenerateFileName();
+                    var savePath = Path.Combine(env.WebRootPath, "img", fileName);
+                    vm.Photo.CopyTo(new FileStream(savePath, FileMode.Create));
+                }
+                var meeting = new Meeting()
+                {
+                    Title = vm.Title,
+                    Description = vm.Description,
+                    MeetingTime = vm.MeetingTime,
+                    Place = vm.Place,
+                    PhotoPath = fileName
+                };
+                _db.Meetings.Add(meeting);
+                _db.SaveChanges();
+            }
+            return View();
+        }
+
+    }
+}
